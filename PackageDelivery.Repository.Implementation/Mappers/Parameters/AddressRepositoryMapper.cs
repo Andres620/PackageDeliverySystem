33ce98d70d57888e292871dd90ac12b06f6da267
CristianDGuerreroA <69438010+CristianDGuerreroA@users.noreply.github.com>
2023-03-23 11:46:00 -0500

    Creación de las clase en mappers, SE DEBEN MODIFICAR.
    Se crearon las clases base para implementar los mappers.

---
+++ PackageDelivery.Repository.Implementation/Mappers/Parameters/AddressRepositoryMapper.cs
@@ -0,0 +1,47 @@
+﻿using PackageDelivery.Repository.Implementation.DataModel;
+using PackageDelivery.Repository.Contracts.DbModels.Parameters;
+using System.Collections.Generic;
+
+namespace PackageDelivery.Repository.Implementation.Mappers.Parameters
+{
+    public class DocumentTypeRepositoryMapper: DbModelMapperBase<DocumentTypeDbModel, tipoDocumento>
+    {
+        public override DocumentTypeDbModel DatabaseToDbModelMapper(tipoDocumento input)
+        {
+            return new DocumentTypeDbModel()
+            {
+                Id = input.id,
+                name = input.nombre
+            };
+        }
+
+        public override IEnumerable<DocumentTypeDbModel> DatabaseToDbModelMapper(IEnumerable<tipoDocumento> input)
+        {
+            IList<DocumentTypeDbModel> list = new List<DocumentTypeDbModel>();
+            foreach (var item in input)
+            {
+                list.Add(this.DatabaseToDbModelMapper(item));
+            }
+            return list;
+        }
+
+        public override tipoDocumento DbModelToDatabaseMapper(DocumentTypeDbModel input)
+        {
+            return new tipoDocumento()
+            {
+                id = input.Id,
+                nombre = input.name
+            };
+        }
+
+        public override IEnumerable<tipoDocumento> DbModelToDatabaseMapper(IEnumerable<DocumentTypeDbModel> input)
+        {
+            IList<tipoDocumento> list = new List<tipoDocumento>();
+            foreach (var item in input)
+            {
+                list.Add(this.DbModelToDatabaseMapper(item));
+            }
+            return list;
+        }
+    }
+}
