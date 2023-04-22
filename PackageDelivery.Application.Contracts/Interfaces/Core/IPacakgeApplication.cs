f66a8c0e3855a964f7985cabeefe63244c0aaea1
CristianDGuerreroA <69438010+CristianDGuerreroA@users.noreply.github.com>
2023-04-22 11:21:00 -0500

    Interfaces creadas

---
+++ PackageDelivery.Application.Contracts/Interfaces/Core/IPacakgeApplication.cs
@@ -0,0 +1,14 @@
+﻿using PackageDelivery.Application.Contracts.DTO.CoreDTO;
+using System.Collections.Generic;
+
+namespace PackageDelivery.Application.Contracts.Interfaces.Core
+{
+    public interface IPackageApplication
+    {
+        PackageDTO getRecordById(int id);
+        IEnumerable<PackageDTO> getRecordsList(string filter);
+        PackageDTO createRecord(PackageDTO record);
+        PackageDTO updateRecord(PackageDTO record);
+        bool deleteRecordById(int id);
+    }
+}
---
+++ PackageDelivery.Application.Contracts/Interfaces/Parameters/IDepartmentApplication.cs
@@ -0,0 +1,14 @@
+﻿using PackageDelivery.Application.Contracts.DTO.ParametersDTO;
+using System.Collections.Generic;
+
+namespace PackageDelivery.Application.Contracts.Interfaces.Parameters
+{
+    public interface IDepartmentApplication
+    {
+        DepartmentDTO getRecordById(int id);
+        IEnumerable<DepartmentDTO> getRecordsList(string filter);
+        DepartmentDTO createRecord(DepartmentDTO record);
+        DepartmentDTO updateRecord(DepartmentDTO record);
+        bool deleteRecordById(int id);
+    }
+}
