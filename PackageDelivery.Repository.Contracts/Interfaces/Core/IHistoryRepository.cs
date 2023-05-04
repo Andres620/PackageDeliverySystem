b4f375d6ac40dd987b579684f0e137dfc2177538
CristianDGuerreroA <69438010+CristianDGuerreroA@users.noreply.github.com>
2023-05-04 08:53:09 -0500

    Interfaces realizadas, resta implementación.

---
+++ PackageDelivery.Repository.Contracts/Interfaces/Core/IHistoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using PackageDelivery.Repository.Contracts.DbModels.Core;
+using System.Collections.Generic;
+
+namespace PackageDelivery.Repository.Contracts.Interfaces.Core
+{
+    public interface IHistoryRepository
+    {
+        HistoryDbModel getRecordById(int id);
+        IEnumerable<HistoryDbModel> getRecordsList(string filter);
+        HistoryDbModel createRecord(HistoryDbModel record);
+        HistoryDbModel updateRecord(HistoryDbModel record);
+        bool deleteRecordById(int id);
+    }
+}
---
+++ PackageDelivery.Repository.Contracts/Interfaces/Core/IShipmentRepository.cs
@@ -0,0 +1,14 @@
+﻿using PackageDelivery.Repository.Contracts.DbModels.Core;
+using System.Collections.Generic;
+
+namespace PackageDelivery.Repository.Contracts.Interfaces.Core
+{
+    public interface IShipmentRepository
+    {
+        ShipmentDbModel getRecordById(int id);
+        IEnumerable<ShipmentDbModel> getRecordsList(string filter);
+        ShipmentDbModel createRecord(ShipmentDbModel record);
+        ShipmentDbModel updateRecord(ShipmentDbModel record);
+        bool deleteRecordById(int id);
+    }
+}
