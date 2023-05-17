24683557f1e4be2c71bdfcf12ccc409282c1c3dc
Andres620 <andres.1701625573@ucaldas.edu.co>
2023-05-17 18:48:50 -0500

    Se actualizaron los controladores de Office - History - Package - Shipment

--- PackageDelivery.GUI/Controllers/Core/PackageController.cs
+++ PackageDelivery.GUI/Controllers/Core/PackageController.cs
@@ -6,6 +6,11 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using PackageDelivery.Application.Contracts.DTO.CoreDTO;
+using PackageDelivery.Application.Contracts.Interfaces.Core;
+using PackageDelivery.Application.Implementation.Implementation.Core;
+using PackageDelivery.GUI.Helpers;
+using PackageDelivery.GUI.Mappers.Core;
 using PackageDelivery.GUI.Models;
 using PackageDelivery.GUI.Models.Core;
 
@@ -13,28 +18,32 @@ namespace PackageDelivery.GUI.Controllers.Core
 {
     public class PackageController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+		private IPackageApplication _app = new PackageImpApplication();
 
-        // GET: Package
-        public ActionResult Index()
+		// GET: Package
+		public ActionResult Index(double filter = 0)
         {
-            return View(db.PackageModels.ToList());
-        }
+			var dtoList = _app.getRecordsList(filter);
+			PackageGUIMapper mapper = new PackageGUIMapper();
+			IEnumerable<PackageModel> model = mapper.DTOToModelMapper(dtoList);
+			return View(model);
+		}
 
         // GET: Package/Details/5
         public ActionResult Details(int? id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            PackageModel packageModel = db.PackageModels.Find(id);
-            if (packageModel == null)
-            {
-                return HttpNotFound();
-            }
-            return View(packageModel);
-        }
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			PackageGUIMapper mapper = new PackageGUIMapper();
+			PackageModel packageModel = mapper.DTOToModelMapper(_app.getRecordById(id.Value));
+			if (packageModel == null)
+			{
+				return HttpNotFound();
+			}
+			return View(packageModel);
+		}
 
         // GET: Package/Create
         public ActionResult Create()
@@ -49,30 +58,40 @@ namespace PackageDelivery.GUI.Controllers.Core
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,weight,height,depth,width,idOffice")] PackageModel packageModel)
         {
-            if (ModelState.IsValid)
-            {
-                db.PackageModels.Add(packageModel);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(packageModel);
-        }
+			if (ModelState.IsValid)
+			{
+				PackageGUIMapper mapper = new PackageGUIMapper();
+				PackageDTO response = _app.createRecord(mapper.ModelToDTOMapper(packageModel));
+				if (response != null)
+				{
+					ViewBag.ClassName = ActionMessages.successClass;
+					ViewBag.Message = ActionMessages.successMessage;
+					return RedirectToAction("Index");
+				}
+				ViewBag.ClassName = ActionMessages.warningClass;
+				ViewBag.Message = ActionMessages.alreadyExistsMessage;
+				return RedirectToAction("Index");
+			}
+			ViewBag.ClassName = ActionMessages.warningClass;
+			ViewBag.Message = ActionMessages.errorMessage;
+			return View(packageModel);
+		}
 
         // GET: Package/Edit/5
         public ActionResult Edit(int? id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            PackageModel packageModel = db.PackageModels.Find(id);
-            if (packageModel == null)
-            {
-                return HttpNotFound();
-            }
-            return View(packageModel);
-        }
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			PackageGUIMapper mapper = new PackageGUIMapper();
+			PackageModel packageModel = mapper.DTOToModelMapper(_app.getRecordById(id.Value));
+			if (packageModel == null)
+			{
+				return HttpNotFound();
+			}
+			return View(packageModel);
+		}
 
         // POST: Package/Edit/5
         // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
@@ -81,48 +100,47 @@ namespace PackageDelivery.GUI.Controllers.Core
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,weight,height,depth,width,idOffice")] PackageModel packageModel)
         {
-            if (ModelState.IsValid)
-            {
-                db.Entry(packageModel).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(packageModel);
-        }
+			if (ModelState.IsValid)
+			{
+				PackageGUIMapper mapper = new PackageGUIMapper();
+				PackageDTO response = _app.updateRecord(mapper.ModelToDTOMapper(packageModel));
+				if (response != null)
+				{
+					return RedirectToAction("Index");
+				}
+			}
+			ViewBag.Message = ActionMessages.errorMessage;
+			return View(packageModel);
+		}
 
         // GET: Package/Delete/5
         public ActionResult Delete(int? id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            PackageModel packageModel = db.PackageModels.Find(id);
-            if (packageModel == null)
-            {
-                return HttpNotFound();
-            }
-            return View(packageModel);
-        }
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			PackageGUIMapper mapper = new PackageGUIMapper();
+			PackageModel packageModel = mapper.DTOToModelMapper(_app.getRecordById(id.Value));
+			if (packageModel == null)
+			{
+				return HttpNotFound();
+			}
+			return View(packageModel);
+		}
 
         // POST: Package/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            PackageModel packageModel = db.PackageModels.Find(id);
-            db.PackageModels.Remove(packageModel);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+			bool response = _app.deleteRecordById(id);
+			if (response)
+			{
+				return RedirectToAction("Index");
+			}
+			ViewBag.Message = ActionMessages.errorMessage;
+			return View();
+		}
     }
 }
