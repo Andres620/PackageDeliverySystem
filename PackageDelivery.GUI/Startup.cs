5e94f032c87fa758dfe46096db8b948f68d8daec
CristianDGuerreroA <69438010+CristianDGuerreroA@users.noreply.github.com>
2023-05-27 12:21:32 -0500

    Fixed errors.

--- PackageDelivery.GUI/Startup.cs
+++ PackageDelivery.GUI/Startup.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using System.Globalization;
+using System.Threading;
+using System;
 
 [assembly: OwinStartupAttribute(typeof(PackageDelivery.GUI.Startup))]
 namespace PackageDelivery.GUI
@@ -9,6 +12,15 @@ namespace PackageDelivery.GUI
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+        }
+
+        public static void Main()
+        {
+            CultureInfo culture = (CultureInfo)CultureInfo.CurrentCulture.Clone();
+            culture.DateTimeFormat.ShortDatePattern = "dd-MMM-yyyy";
+            culture.DateTimeFormat.LongTimePattern = "";
+            Thread.CurrentThread.CurrentCulture = culture;
+            Console.WriteLine(DateTime.Now);
         }
     }
 }
